f2f6186f12211b79c4fc59ee60407e871fa663b9
Kristjan Kask <kristjanvankask@gmail.com>
2022-01-04 18:30:58 +0200

    Added comments, removed unnecessary white space

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Product, Order, OrderItem
 
+# Adding models to admin panel
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
